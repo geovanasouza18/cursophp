c9dd11c928a7681d3b80727efd4f263a78f7142b
Geovana Souza <geovanasouza0712@gmail.com>
2024-01-09 22:00:20 -0300

    novo exercicio

---
+++ exercicios/ex010/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manipulação de strings</title>
+</head>
+<body>
+    <h1>Manipulação de strings</h1>
+<?php 
+    $nome = "BOM DIA";
+
+    echo strtoupper($nome);
+
+    ?>
+</body>
+</html>
