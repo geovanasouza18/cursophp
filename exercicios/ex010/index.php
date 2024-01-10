0b0500d2edfd0f0cb05c869cfd80c6758329f02c
Geovana Souza <geovanasouza0712@gmail.com>
2024-01-09 22:16:00 -0300

    atualizando

--- exercicios/ex010/index.php
+++ exercicios/ex010/index.php
@@ -8,9 +8,37 @@
 <body>
     <h1>Manipulação de strings</h1>
 <?php 
-    $nome = "BOM DIA";
+    $nome = "Bom dia";
 
+    //tudo maiusculo
     echo strtoupper($nome);
+
+    echo '<br>';
+        //Primeira letra de cada palavra maiscula
+    echo ucwords($nome);
+
+    echo '<br>';
+        //Primeira letra maiscula
+    echo ucfirst($nome);
+
+    echo '<br>';
+            //tudo minusculo
+    echo strtolower($nome);
+
+    $mudar = "Boa tarde";
+    echo '<br>';
+    //Mudar palavra
+    echo str_replace('tarde', 'noite', $mudar);
+
+    echo '<br>';
+    //Comecar a imprimir
+    echo substr($mudar,3);
+
+    echo '<br>';
+    //Número do comprimento do texto, bom para utilizar para CPF, RG. Conta com o espaço vazio
+    echo strlen($mudar);
+
+
 
     ?>
 </body>
