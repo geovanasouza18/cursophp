1f6ab38d79646801a2406742c423203e981277d1
Geovana Souza <geovanasouza0712@gmail.com>
2024-01-11 22:15:48 -0300

    Niv

---
+++ desafios/desafio06/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercícios</title>
+</head>
+<body>
+    <h1>Aprovado ou reprovado?</h1>
+    <?php 
+        $nota = 7;
+        if ($nota >=7) {
+                echo "Está aprovado!";
+        }
+        else {
+            echo "Está reprovado!";
+        }
+
+        
+    ?>
+</body>
+</html>
